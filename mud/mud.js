d4f08d1339d135753d4391d9101da39be7cd959f
anagram-of-pi <58116783+anagram-of-pi@users.noreply.github.com>
2026-06-05 14:39:26 -0700

    Added some comments

---
+++ mud/mud.js
@@ -0,0 +1,9 @@
+/* 
+TODO:
+[ ] Make a placeholder API to call to get the (dummy) information
+[ ] Make it dynamically create the pages and items
+[ ] Make it so that clicking a channel or page or dm updates the current `content type` and `page id`
+[ ] Display the content in the content panel
+[ ] Send data about changes and messages back to API
+*/
+
